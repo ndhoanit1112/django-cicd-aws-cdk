912fed70020f0933ef28013dfe568574b517d4df
Nguyen Dinh Hoan <ndhoanit1112@gmail.com>
2023-06-16 09:58:46 +0900

    Change app name

--- test/django-cicd-aws-cdk.test.ts
+++ test/django-cicd-aws-cdk.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as DjangoDeployCdk from '../lib/django-deploy-cdk-stack';
+// import * as DjangoDeployCdk from '../lib/django-cicd-aws-cdk-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/django-deploy-cdk-stack.ts
+// example resource in lib/django-cicd-aws-cdk-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
